a7d80b84fd2d4cfba8ab72b0b6be00f5c3d5e52e
Ali Shah <s.ashah@outlook.com>
2021-08-31 15:08:22 -0400

    [Finished] challenges 2-4

--- src/data.js
+++ src/data.js
@@ -1,3 +1,16 @@
 import data from './data.json';
 
-export default data;
+const categories = data.map(product => product.category);
+
+const categoriesUnique = [...new Set(categories)];
+
+const categoriesCount = data.reduce((obj, product) => {
+  if (obj[product.category]) {
+    obj[product.category] += 1
+    return obj
+  }
+  obj[product.category] = 1
+  return obj
+}, {});
+
+export default categoriesUnique;
