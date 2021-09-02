54311a44820e099cc82333be73e6c1035b2ee534
Ali Shah <s.ashah@outlook.com>
2021-09-01 20:51:52 -0400

    [Finished] challenge 9

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ function Product(props) {
   return (
     <div className="Product">
       <h1>{props.name}</h1>
-      <h1>{props.price}</h1>
+      <h2>{props.price}</h2>
       <p>{props.category}</p>
     </div>
   );
